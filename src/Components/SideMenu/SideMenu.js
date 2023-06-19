63a41831687cf4efe870e073e4c64dddf647adcf
Sushant Kumar <sushantkumar@Sushants-MacBook-Air.local>
2023-06-19 13:34:03 +0530

    first commits

---
+++ src/Components/SideMenu/SideMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SideMenu = () => {
+  return (
+    <div className="side-menu">
+       <p>Home</p>
+       <p>logout</p>
+    </div>
+  );
+}
+
+export default SideMenu;
